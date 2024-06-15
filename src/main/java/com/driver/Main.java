f7bb41a0c117eee6c96563d0c09a28a0835a98ec
amolgawai10 <amolgawai78@gmail.com>
2024-06-15 19:17:15 +0530

    Encapsulation Assignment Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+          RWOnly a = new RWOnly();
+//        a.name = "Amol";
+//        System.out.println(a.name);
+        a.setName("Amol");
+        System.out.println(a.getName());
+    }
 }
